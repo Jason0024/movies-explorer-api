c7704eecd21c7755bf710cbda857cd851b17d3db
Sergey Shamardin <s.shamrdin@gmail.com>
2023-07-18 20:31:24 +0700

    fix/ auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,14 +4,14 @@ const Unauthorized = require('../utils/response-errors/Unauthorized');
 const { NODE_ENV, JWT_SECRET } = process.env;
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const token = req.headers.authorization;
 
   if (!token) { return next(new Unauthorized('Для доступа необходимо выполнить авторизацию')); }
 
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'diploma-secret');
+    payload = jwt.verify(token.replace("Bearer ", ""), NODE_ENV === 'production' ? JWT_SECRET : 'diploma-secret');
   } catch (_) { return next(new Unauthorized('Для доступа необходимо выполнить авторизацию')); }
 
   req.user = payload;
